6ded4c9d93d9b0fb9eed2e9383d2046839b73806
solacens <yippw1025@gmail.com>
2019-01-16 17:09:42 +0800

    remove unworkable rb

--- brew-tap.rb
+++
@@ -1,8 +0,0 @@
-require 'formula'
-
-class BrewMycask < Formula
-  homepage 'https://github.com/plutux-labs/homebrew-tap/'
-  url 'https://github.com/plutux-labs/homebrew-tap.git'
-  def install
-  end
-end
