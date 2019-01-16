e19e8d429e695c5cde3b4310c93df006d0e5c5b6
solacens <yippw1025@gmail.com>
2019-01-16 17:11:55 +0800

    edited rb

--- kubernetes-utility.rb
+++ kubernetes-utility.rb
@@ -1,11 +1,13 @@
-class KubernetesUtility < Cask
+cask 'kubernetes-utility' do
   url 'https://github.com/plutux-labs/kubernetes-utility/releases/download/v1.0.0/Kubernetes.Utility.Install.dmg'
+  name 'Kubernetes Utility'
   homepage 'kubernetes-utility'
+
   version '1.0.0'
   sha256 'f1eba61ad9d18dd22998fe8507d086dd1b8a3356d9d0824fa4ef5b6594bbc24e'
 
   depends_on formula: 'kubernetes-cli'
   depends_on formula: 'johanhaleby/kubetail'
 
-  link 'Kubernetes Utility.app'
+  app 'Kubernetes Utility.app'
 end
