4ba88b28cd0f6ddd64481662a117303740ff4b1e
solacens <yippw1025@gmail.com>
2019-04-08 17:18:53 +0800

    update cask

--- Casks/kubernetes-utility.rb
+++ Casks/kubernetes-utility.rb
@@ -1,10 +1,10 @@
 cask 'kubernetes-utility' do
-  url 'https://github.com/plutux-labs/kubernetes-utility/releases/download/v1.0.0/Kubernetes.Utility.Install.dmg'
+  url 'https://github.com/plutux-labs/kubernetes-utility/releases/download/v1.1.0/Kubernetes.Utility.Install.dmg'
   name 'Kubernetes Utility'
   homepage 'kubernetes-utility'
 
-  version '1.0.0'
-  sha256 'f1eba61ad9d18dd22998fe8507d086dd1b8a3356d9d0824fa4ef5b6594bbc24e'
+  version '1.1.0'
+  sha256 '22ba1190da3438d95e0eebbff2fdfcbe7b7b890107888f46b918957160c38746'
 
   depends_on formula: 'kubernetes-cli'
   depends_on formula: 'johanhaleby/kubetail/kubetail'
